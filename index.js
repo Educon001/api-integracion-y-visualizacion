d863a93987b91c96b134539f8476ac5f2ba6f770
Eduardo Consalvo <educon001@gmail.com>
2023-06-11 01:18:46 -0400

    Fix port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const app = express();
 
+const port = process.env.PORT || 3000;
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -82,6 +83,6 @@ app.get('/fetch_data', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
